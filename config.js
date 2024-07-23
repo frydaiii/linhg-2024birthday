681b87eae0ef286486b9045cbfbce2da79b97ab6
Trần Đức Mạnh <66522548+frydaiii@users.noreply.github.com>
2024-07-23 10:27:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Chúc babi của anh",     
+        "Bước sang tuổi mới",  
+        "Luôn xinh đẹp rạng ngời",
+        "Chăm ngoan học giỏi",
+        "Công việc như ý",
+        "Ăn uống đầy đủ",
+        "Ngủ nghỉ ngon giấc",
+        "Khỏe mạnh và bình an",
+        "Chúc babi mọi điều tốt đẹp trên thế giới nàyyyyy",
+        "Anh thương em nhiềuuuuu",
+        "CHỤT~~",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "Bật đèn nè",
+        play: "Bật nhạc nè",
+        bannar_coming: "Trang trí nè",
+        balloons_flying: "Bóng bay nè",
+        cake_fadein: "Tặng babi cí bánh nè",
+        light_candle: "Babi thắp nến i",
+        wish_message: "Happy birthday babiii",
+        story: "Lời chúc bí mật",
     }
 };
